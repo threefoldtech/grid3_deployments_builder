4cd9a1be74ea09e4f0bc0e22b80c86010f652927
MohamedElmdary <engm5081@gmail.com>
2021-10-10 11:32:13 +0200

    solved conflicts issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,85 @@
 <script lang="ts">
   import codeStore from "./store/code.store";
   import Builder from "./components/Builder.svelte";
-  import ProjectDisplay from "./components/ProjectDisplay.svelte";
+  import TerraForm from "./components/TerraForm.svelte";
   import FloatingActions from "./components/FloatingActions.svelte";
-
   let explorer = 0;
   const setExplorer = (x: number) => () => (explorer = x);
-
+  import { loadFromFile, dumpToFile } from "grid3_client_ts";
+  dumpToFile("/xx", "aa");
+  console.log(loadFromFile("/xx"));
   $: store = $codeStore;
 </script>
+
+<style lang="scss" scoped>
+  main {
+    padding-left: var(--sidenav-width);
+    height: 100%;
+    background-color: var(--ground-bg);
+  }
+  .sidenav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: var(--sidenav-width);
+    display: flex;
+    border: 0.1rem solid var(--sidenav-border);
+    &__actions {
+      width: 6rem;
+      border: 0.1rem solid var(--sidenav-border);
+      &__item {
+        width: 6rem;
+        height: 6rem;
+        border: none;
+        background: none;
+        cursor: pointer;
+        &:hover,
+        &.active {
+          background-color: #e9e9e9;
+        }
+        > img {
+          width: 90%;
+          max-width: 4.5rem;
+        }
+      }
+    }
+    &__content {
+      width: calc(100% - 6rem);
+      height: 100%;
+      overflow-x: hidden;
+      overflow-y: auto;
+      &__create {
+        width: 100%;
+        padding: 2rem;
+        cursor: pointer;
+        margin-bottom: 1rem;
+      }
+      &__project {
+        width: 100%;
+        padding: 1.5rem;
+        background: none;
+        border: none;
+        border-bottom: 0.1rem solid var(--sidenav-border);
+        cursor: pointer;
+        &.active {
+          background-color: #e9e9e9;
+        }
+      }
+    }
+  }
+</style>
 
 <aside class="sidenav">
   <div class="sidenav__actions">
     <button
       on:click={setExplorer(0)}
-      class={"sidenav__actions__item " + (explorer === 0 ? "active" : "")}
-    >
+      class={'sidenav__actions__item ' + (explorer === 0 ? 'active' : '')}>
       <img src="/assets/file.svg" alt="explorer" title="file explorer" />
     </button>
     <button
       on:click={setExplorer(1)}
-      class={"sidenav__actions__item " + (explorer === 1 ? "active" : "")}
-    >
+      class={'sidenav__actions__item ' + (explorer === 1 ? 'active' : '')}>
       <img src="/assets/tools.svg" alt="builder" title="resource builder" />
     </button>
   </div>
@@ -29,22 +87,18 @@
     {#if explorer === 0}
       <button
         class="sidenav__content__create"
-        on:click={codeStore.add.bind(codeStore, "project")}
-      >
+        on:click={codeStore.add.bind(codeStore, 'project')}>
         Add New Project
       </button>
       {#each store.projects as project, i}
         <button
-          class={"sidenav__content__project " +
-            (store.active === i ? "active" : "")}
-          on:click={codeStore.setActiveProject.bind(codeStore, i)}
-        >
+          class={'sidenav__content__project ' + (store.active === i ? 'active' : '')}
+          on:click={codeStore.setActiveProject.bind(codeStore, i)}>
           <strong>#{i}</strong>
           {project.name}
           <span
             style="display: inline-block; margin-left: 20px; font-size: 1.5rem;"
-            on:click={codeStore.removeProject.bind(codeStore, i)}
-          >
+            on:click={codeStore.removeProject.bind(codeStore, i)}>
             X
           </span>
         </button>
@@ -55,74 +109,6 @@
   </div>
 </aside>
 <main>
-  <ProjectDisplay />
+  <TerraForm />
 </main>
 <FloatingActions />
-
-<style lang="scss" scoped>
-  main {
-    padding-left: var(--sidenav-width);
-    height: 100%;
-    background-color: var(--ground-bg);
-  }
-
-  .sidenav {
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: var(--sidenav-width);
-    display: flex;
-    border: 0.1rem solid var(--sidenav-border);
-
-    &__actions {
-      width: 6rem;
-      border: 0.1rem solid var(--sidenav-border);
-
-      &__item {
-        width: 6rem;
-        height: 6rem;
-        border: none;
-        background: none;
-        cursor: pointer;
-
-        &:hover,
-        &.active {
-          background-color: #e9e9e9;
-        }
-
-        > img {
-          width: 90%;
-          max-width: 4.5rem;
-        }
-      }
-    }
-
-    &__content {
-      width: calc(100% - 6rem);
-      height: 100%;
-      overflow-x: hidden;
-      overflow-y: auto;
-
-      &__create {
-        width: 100%;
-        padding: 2rem;
-        cursor: pointer;
-        margin-bottom: 1rem;
-      }
-
-      &__project {
-        width: 100%;
-        padding: 1.5rem;
-        background: none;
-        border: none;
-        border-bottom: 0.1rem solid var(--sidenav-border);
-        cursor: pointer;
-
-        &.active {
-          background-color: #e9e9e9;
-        }
-      }
-    }
-  }
-</style>
